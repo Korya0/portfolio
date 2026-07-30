ea98014929897378db19b43396b2bca229c6469d
mahmoudmohamed <mahmoudmohamed01559@gmail.com>
2026-07-30 22:05:01 +0300

    feat: implement dynamic technology stack section with categorized SVG icons and scripts

--- js/tech.js
+++ js/tech.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function mapTechData() {
     let tech = [
+        // Mobile Development (Longest → Shortest)
         {
             "name": "Flutter",
             "icon": "images/tech/flutter.svg",
@@ -14,26 +15,100 @@ function mapTechData() {
             "icon": "images/tech/dart.svg",
             "type": "mobile-tech"
         },
+
+        // Networking (Longest → Shortest)
         {
             "name": "REST APIs",
             "icon": "images/tech/api.svg",
             "type": "server-tech"
         },
         {
+            "name": "GraphQL",
+            "icon": "images/tech/graphql.svg",
+            "type": "server-tech"
+        },
+
+        // Databases (Longest → Shortest)
+        {
             "name": "Firebase",
             "icon": "images/tech/firebase.svg",
             "type": "database-tech"
         },
         {
-            "name": "GitHub",
-            "icon": "images/tech/github.svg",
-            "type": "vc-tech"
+            "name": "Supabase",
+            "icon": "images/tech/supabase.svg",
+            "type": "database-tech"
         },
+
+        // State Management (Longest → Shortest)
+        {
+            "name": "Bloc / Cubit",
+            "icon": "images/tech/bloc-svgrepo-com.svg",
+            "type": "state-tech"
+        },
+        {
+            "name": "Riverpod",
+            "icon": "images/tech/redux-svgrepo-com.svg",
+            "type": "state-tech"
+        },
+
+        // Local Storage (Longest → Shortest)
+        {
+            "name": "SharedPreferences",
+            "icon": "images/tech/storage.svg",
+            "type": "storage-tech"
+        },
+        {
+            "name": "Hive",
+            "icon": "images/tech/hive.svg",
+            "type": "storage-tech"
+        },
+
+        // Testing (Longest → Shortest)
+        {
+            "name": "Integration",
+            "icon": "images/tech/testing.svg",
+            "type": "testing-tech"
+        },
+        {
+            "name": "Widget",
+            "icon": "images/tech/testing.svg",
+            "type": "testing-tech"
+        },
+        {
+            "name": "Unit",
+            "icon": "images/tech/testing.svg",
+            "type": "testing-tech"
+        },
+
+        // CI/CD (Longest → Shortest)
+        {
+            "name": "GitHub Actions",
+            "icon": "images/tech/github-actions.svg",
+            "type": "cicd-tech"
+        },
+        {
+            "name": "Fastlane",
+            "icon": "images/tech/fastlane.svg",
+            "type": "cicd-tech"
+        },
+        {
+            "name": "GitHub",
+            "icon": "images/tech/github-actions.svg",
+            "type": "cicd-tech"
+        },
+        {
+            "name": "Git",
+            "icon": "images/tech/git.svg",
+            "type": "cicd-tech"
+        },
+
+        // Design & Tools
         {
             "name": "Figma",
             "icon": "images/tech/figma.svg",
             "type": "design-tech"
-        }
+        },
     ];
 
     for (var i = 0; i < tech.length; i++) {
